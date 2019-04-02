4d7fc56e95984f402f801c655b361ef42c648435
yanghao <hao.yang6@renren-inc.com>
2019-04-02 19:29:36 +0800

    remove blank

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('../db')
 var schema1 = new mongoose.Schema({ username: String, password: String })
-  var model1 = mongoose.model('model1', schema1, 'demo')
+var model1 = mongoose.model('model1', schema1, 'demo')
 router.get('/test', function (req, res, next) {
   model1.find().then(d => {
     res.send(d)
