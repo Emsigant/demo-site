c609e0cfc9728ac936deed54b40e5eaccc4443c6
yanghao <hao.yang6@renren-inc.com>
2019-06-13 14:59:25 +0800

    code modify

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const DB_URL = process.argv.indexOf('DEV') >=0 ? 'mongodb://116.62.126.60:27017/demo':'mongodb://localhost:27017/demo'
+const IS_DEV = process.argv.indexOf('DEV') >= 0 ? 'mongodb://116.62.126.60:27017/demo' : 'mongodb://localhost:27017/demo'
+const DB_URL = `mongodb://${IS_DEV ? `116.62.126.60` : `localhost`}:27017/demo`
 async function initmongoose() {
     try {
         await mongoose.connect(DB_URL, { useNewUrlParser: true })
