d766643b13d478bd283a73c6a245c69c0b83f63c
unknown <emsigant@gmail.com>
2019-07-18 00:03:55 +0800

    login api

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,43 @@ router.get('/test', function (req, res, next) {
 });
 
 router.get('/body', function (req, res, next) {
-  res.send(req.params);
+  res.send(req.body);
 });
+
+router.post('/login', async function (req, res, next) {
+  const { username, password } = req.body;
+  if (username && password) {
+    model1
+      .find({ username })
+      .then(result => {
+        if (result.length) {
+          const match = result[0];
+          if (match.password === password) {
+            res.send({
+              message: 'Login success'
+            });
+          } else {
+            res.send({
+              message: 'Wrong password'
+            });
+          }
+        } else {
+          res.send({
+            message: 'Account not exist'
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.send({
+          message: 'Fail to fetch data from database'
+        });
+      });
+  } else {
+    res.send({
+      message: 'Bad request'
+    });
+  }
+})
 
 module.exports = router;
